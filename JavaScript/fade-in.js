26055df5c7eb55bd5a7448b454d657422c493435
ryang <ryan.r.c.339ac@gmail.com>
2021-11-04 13:14:57 -0300

    created js part of fade-in, changed css properties and applied it in what i do

--- JavaScript/fade-in.js
+++ JavaScript/fade-in.js
@@ -1,1 +1,24 @@
 const faders = document.querySelectorAll('.fade-in')
+
+const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -100px 0px"
+}
+
+const appearOnScrool = new IntersectionObserver(
+    (entries, appearOnScrool) => {
+        entries.forEach( entry => {
+            if(!entry.isIntersecting){
+                return
+            }
+            else{
+                entry.target.classList.add('appear')
+                appearOnScrool.unobserve(entry.target)
+            }
+
+        })
+    }, appearOptions
+)
+faders.forEach(fader => {
+    appearOnScrool.observe(fader)
+})
